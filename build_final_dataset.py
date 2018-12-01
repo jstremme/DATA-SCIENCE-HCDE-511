625779dc9b92cadea1618e556f3e8fc54c9fe8e1
Stremmel <jstremme@LAMU02T9E48GTFM.uhc.com>
2018-12-01 15:16:31 -0800

    add final dataset and code for construction

---
+++ build_final_dataset.py
@@ -0,0 +1,27 @@
+import pandas as pd
+pd.set_option('display.max_columns', 50)
+
+tweets = ['economy_from:' + x for x in ['FoxNews', 'MSNBC', 'CNN', 'realDonaldTrump']]
+tweet_dfs = []
+for tweet_source in tweets:
+	df = pd.read_csv('dashboard_archive/{}.csv'.format(tweet_source))
+	tweet_dfs.append(df)
+
+all_tweets = pd.concat(tweet_dfs, ignore_index=True)
+all_tweets = all_tweets.drop_duplicates()
+
+stocks = ['^DJI', '^GSPC', 'AMZN', 'XOM']
+stock_dfs = []
+for stock in stocks:
+	df = pd.read_csv('stock_archive/{}.csv'.format(stock))
+	df['Index'] = stock
+	stock_dfs.append(df)
+
+all_stocks = pd.concat(stock_dfs, ignore_index=True)
+all_stocks = all_stocks.drop_duplicates()
+
+all_stocks = all_stocks[['Date', 'Close', 'Index']]
+all_stocks = all_stocks.rename(index=str, columns={"Date": "day_timestamp"})
+
+final_df = pd.merge(all_stocks, all_tweets, on='day_timestamp', how='left')
+final_df.to_csv('final_datasets/tableau_input1.csv', index=False)
